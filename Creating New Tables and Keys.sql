cd6dc93cb06460e7e288e6715346b8fe3b58e0dd
harveyjoyce <harveyjoyce56@outlook.com>
2024-03-08 15:39:19 +0000

    Initial commit

---
+++ Creating New Tables and Keys.sql
@@ -0,0 +1,110 @@
+CREATE TABLE athletes  (
+  athlete_id INT,
+  name VARCHAR(100),
+  sex VARCHAR(1),
+  height INT,
+  weight INT
+);
+INSERT INTO athletes
+SELECT DISTINCT
+id as athlete_id,
+name,
+sex,
+NULLIF(height,'') as height,
+NULLIF(weight,'') as weight
+FROM olympics.staging;
+
+CREATE TABLE teams (
+  team_id INT,
+  team VARCHAR(50),
+  NOC VARCHAR(3),
+  NOC_region VARCHAR(50),
+  NOC_notes VARCHAR(30)
+);
+INSERT INTO teams
+with teams as (SELECT DISTINCT 
+	team,
+	noc,
+	noc_region,
+	NOC_notes
+	FROM staging)
+SELECT 
+ROW_NUMBER() OVER(ORDER BY team) as team_id,
+team,
+noc,
+noc_region,
+NOC_notes
+from teams;
+
+CREATE TABLE games (
+  games_id INT,
+  year INT,
+  games VARCHAR(20),
+  season VARCHAR(6),
+  city VARCHAR(25)
+);
+INSERT INTO games
+with games as (SELECT DISTINCT 
+	year,
+	games,
+	season,
+	city
+	FROM staging)
+SELECT 
+ROW_NUMBER() OVER(ORDER BY year, games, season) as games_id,
+year,
+games,
+season,
+city
+FROM games;
+
+CREATE TABLE events (
+  event_id INT,
+  event VARCHAR(100),
+  sport VARCHAR(25)
+);
+INSERT INTO events
+SELECT 
+ROW_NUMBER() OVER(ORDER BY event, sport) as event_id,
+event,
+sport
+FROM (
+	SELECT DISTINCT 
+	event,
+	sport
+	FROM staging
+) as events;
+
+CREATE TABLE results (
+  athlete_id INT,
+  athlete_age INT,
+  team_id INT,
+  games_id INT,
+  event_id INT, 
+  medal VARCHAR(6)
+);
+INSERT INTO results
+SELECT DISTINCT
+id as athlete_id,
+NULLIF(age,'') as athlete_age,
+team_id,
+games_id,
+event_id,
+medal
+FROM staging as S
+INNER JOIN teams as T on T.team = S.team
+INNER JOIN games as G on G.games = S.games AND G.year = S.year AND G.season = S.season
+INNER JOIN events as E on E.event = S.event AND E.sport = S.sport;
+
+
+-- Set Primary Keys
+ALTER TABLE athletes ADD PRIMARY KEY(athlete_id);
+ALTER TABLE teams ADD PRIMARY KEY(team_id);
+ALTER TABLE games ADD PRIMARY KEY(games_id);
+ALTER TABLE events ADD PRIMARY KEY(event_id);
+
+-- Set Foreign Keys
+ALTER TABLE results ADD FOREIGN KEY (athlete_id) REFERENCES athletes(athlete_id);
+ALTER TABLE results ADD FOREIGN KEY (team_id) REFERENCES teams(team_id);
+ALTER TABLE results ADD FOREIGN KEY (games_id) REFERENCES games(games_id);
+ALTER TABLE results ADD FOREIGN KEY (event_id) REFERENCES events(event_id);
